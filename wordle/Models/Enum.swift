59f2516a9d55a51f521cd8b90ace1d15e6443b18
tadahiroueta <tadahiroueta@gmail.com>
2024-05-31 20:04:01 -0500

    remaking enum that got lost

---
+++ wordle/Models/Enum.swift
@@ -0,0 +1,15 @@
+//
+// Enum.swift
+// wordle
+//
+// Created by Lucas Ueta on 5/31/24.
+//
+
+import Foundation
+
+enum TileStatus {
+    case rightPlace
+    case rightLetter
+    case wrong
+    case unknown
+}
